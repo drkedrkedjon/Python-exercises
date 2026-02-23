940fb3ad689bb79f88c577d7d81c5dfd4155772f
Sasha S.P. <sasa@bubulazy.com>
2026-02-23 18:19:37 +0100

    Sets

--- tuples.py
+++ tuples.py
@@ -28,13 +28,19 @@ mi_tuple = mi_tuple[1:] # primero
 arr_tuple = list(mi_tuple)
 arr_tuple.pop(2)
 mi_tuple = tuple(arr_tuple)
-print(mi_tuple)
+# print(mi_tuple)
 
 #--------- Usar un tuple como una KEY en un dictionario
+priority_index = {
+  (1, "premier"): [1,2,3],
+  (1, "deluxe"): [4,5,6],
+  (2, "standard"): [7,8,9],
+}
+# print(list(priority_index.keys()))
 
-
-#--------- 
-
-#--------- 
-
-#--------- 
+#--------- Uso de la function ZIP para unir listas en una tuple
+personas = ["SS", "CGR", "CPG"]
+pasta = [999, 3453, 2433]
+riquesa = list(zip(personas, pasta))
+# print(riquesa)
+# Cuando usas zip, crea un iterador—un objeto especial que genera sus valores uno por uno a medida que lo usas. Una vez que recorres todos sus valores (por ejemplo, convirtiéndolo en una lista o iterando sobre él), queda vacío y no se puede usar de nuevo.
