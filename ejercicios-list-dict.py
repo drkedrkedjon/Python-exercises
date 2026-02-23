e46c33bff5ab29daf289b778e22436286a2331ca
Sasha S.P. <sasa@bubulazy.com>
2026-02-23 12:27:04 +0100

    Python exercises 2

--- ejercicios-list-dict.py
+++ ejercicios-list-dict.py
@@ -19,14 +19,26 @@ my_dictionary = {
 my_ceil = math.ceil(my_decimal)
 
 # Exercise 3: Get the square root of your float.
-my_sqr = my_ceil ** 2
-math.pow(my_ceil, 2)
+math.sqrt(my_ceil)
 
 # Exercise 4: Select the first element from your dictionary.
 primer_elemento = my_dictionary["españa"]
-print(primer_elemento)
+primer_elemento_get = my_dictionary.get("españa", "No hay tal elemento en este dictionario")
+
 # Exercise 5: Select the second element from your tuple.
+segundo_tuple_elem = my_tuple[1]
+
 # Exercise 6: Add an element to the end of your list.
+my_list.append("four")
+my_list.extend(["fifth"])
+
 # Exercise 7: Replace the first element in your list.
+my_list[1] = "hahaha"
+my_list.insert(0, "largate de aqui")
+
 # Exercise 8: Sort your list alphabetically.
+my_list.sort()
+
 # Exercise 9: Use reassignment to add an element to your tuple.
+my_tuple += ("reasignado",)
+print(my_tuple)
