aff235d26be234a230ca8c061b09924f3e3cd4ce
Sasha S.P. <sasa@bubulazy.com>
2026-02-26 12:16:29 +0100

    Conditionales 4

--- M3-condicionales.py
+++ M3-condicionales.py
@@ -2,7 +2,6 @@
 
 # --------
 age = 75
-
 if age < 25:
   print("No puedes alquillar un coche niñato de mierda")
 elif age < 65:
@@ -14,14 +13,23 @@ else:
 role = "admin"
 auth = "Puede ver panel" if role == "admin" else "No puedes ver el panel idiota"
 print(auth)
+
 # -------- Full list of conditional operators
 # ==, !=, >, >=, <, <= --> tambien hay IS que es parecido a === en JS
 uno = []
 dos = []
 print(uno is dos)
 
+# -------- Verificar si una valor esta presente en una List o String
+frase = "Sasa es un cabronazo sin limite"
+if "Sasa" in frase:
+  print("Esta")
+else:
+  print("No esta")
 
-# --------
-
-
+nums = [1, 2, 3, 4]
+if 5 in nums:
+  print("Esta")
+else:
+  print("No esta")
 # --------
