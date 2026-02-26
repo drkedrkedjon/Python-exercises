50ffce201404e4013b94ce0c2b0e8128b1110a21
Sasha S.P. <sasa@bubulazy.com>
2026-02-26 16:08:41 +0100

    Functiones en PY

--- metodos_y_functiones.py
+++ metodos_y_functiones.py
@@ -7,8 +7,14 @@ def mi_function(arg_uno, arg_two):
 mi_function("pinga", "coño")
 
 # -------- Nidar functiones
+def printer(arg1, arg2):
+  def mi_function():
+    return f"{arg1} {arg2}"
+  print(mi_function())
 
+printer("Hola", "Caracola")
 # --------
+
 
 # --------
 
