58e8a83dba0c00e74e76515cf90584f91d997fc2
Sasha S.P. <sasa@bubulazy.com>
2026-02-26 14:22:39 +0100

    Functiones en PY

---
+++ metodos_y_functiones.py
@@ -0,0 +1,25 @@
+# ############## METHODOS Y FUNCTIONES EN PY
+
+# -------- Crear una function
+def mi_function(arg_uno, arg_two):
+  return f"{arg_uno} {arg_two}"
+
+mi_function("pinga", "coño")
+
+# -------- Nidar functiones
+
+# --------
+
+# --------
+
+# --------
+
+# --------
+
+# --------
+
+# --------
+
+# --------
+
+# --------
