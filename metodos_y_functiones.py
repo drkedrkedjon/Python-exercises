7fdc5dac89a485be37f4a0677195ef434bbb5827
Sasha S.P. <sasa@bubulazy.com>
2026-02-26 16:49:42 +0100

    Functiones en PY

--- metodos_y_functiones.py
+++ metodos_y_functiones.py
@@ -13,10 +13,28 @@ def printer(arg1, arg2):
   print(mi_function())
 
 printer("Hola", "Caracola")
-# --------
+
+# -------- Default arguments in JS
+def feee (arg = "FALTA ARGUMENTO IDIOTA"): # Same as in JS
+  print(arg)
+feee("sasa")
+
+# Eso es un problema en PY porque usando lista o dictionario como default da error mas alla cuando reutilizamos la function, siempre se refiere al misma lista u no crea una nueva como en JS
+def funct(arg = []):
+  arg.append(1)
+  return arg
+
+print(funct())
+print(funct())
+
+# Named arguments no tienen estar en orden de argumentos en la function, escribiendo asi se asignan corectamente.
+def fuuuunct(apellido, nombre ):
+  print(f"{nombre} {apellido}")
+
+fuuuunct(nombre = "Sasa", apellido = "Gonzales")
 
 
-# --------
+# -------- 
 
 # --------
 
