58843aa67486e3457201d4c86ea062764c4aba17
Sasha S.P. <sasa@bubulazy.com>
2026-02-27 13:10:58 +0100

    Modulos en PY 1

--- import_modulos_in_py.py
+++ import_modulos_in_py.py
@@ -4,6 +4,11 @@
 import math
 print(math.sqrt(3))
 
-# ---------- Importacion de function desde el otro modulo / documento
+# ---------- Importacion de function desde el otro modulo y TAMBIEN importacion de TODAS las functiones del dicho modulo
 from modulos.helper import greeting
+import modulos.helper
 print(greeting("Sasha"))
+print(modulos.helper.greeting("Sasha"))
+print(modulos.helper.shitting("Sasha"))
+
+# ---------- 
--- modulos/helper.py
+++ modulos/helper.py
@@ -1,2 +1,5 @@
 def greeting(nombre):
   return f"Hola, {nombre} caracola !!!"
+
+def shitting(nombre):
+  return f"Hola, {nombre} estas cagando gilipolla !!!"
