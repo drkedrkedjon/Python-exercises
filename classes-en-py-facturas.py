9f8fb8e4482f4e1aa2942a695736fba2a51cb579
Sasha S.P. <sasa@bubulazy.com>
2026-03-04 13:29:39 +0100

    Clases en PY - property decorators

--- classes-en-py-facturas.py
+++ classes-en-py-facturas.py
@@ -9,24 +9,40 @@ test = Invoice1()
 
 # Las clases se componen de datos y comportamientos
 # Para añadir data usamo constructor
-class Invoice:
+class Invoice2:
   def __init__(self, client, total): # THIS works but is not to be used, use SELF
     self.client = client
     self.total = total
   
-  def client_seter(self, name):
+  def cambiar_nombre(self, name):
     self.client = name.upper()
   
   def printer(self):
     return f"This is {self.client} factura y debe pagar {self.total}"
 
-cafler = Invoice("Pepe Rodriguez", "235€")
-print(cafler.printer())
+cafler = Invoice2("Pepe Rodriguez", "235€")
+cafler._client = "sAsa sAvic"
+cafler.cambiar_nombre("pEndejo gOnzales")
 
-cafler.client = "sAsa sAvic"
-print(cafler.printer())
-
-cafler.client_seter("pEndejo gOnzales")
-print(cafler.printer())
 
 # GET and SET data in python class
+# Se tiene acceso directo a propiedades o variables dentro del objeto creado de una class obj.name y obj.name = name2
+# Las functiones SETTER y GETTER (JS) son necesarias solamente en el caso de necesitar una logica mas compleja.
+
+# The property decorator!
+class Invoice:
+  def __init__(self, client, total): # THIS works but is not to be used, use SELF
+    self._client = client # un _ significa PROTECTED, Igual como en JS. No cambia nada, solo forma de declarar.
+    self._total = total
+
+  # Property decorator sirve para acudir luego a data PROTEGIDA
+  @property
+  def client(self):
+      return self._client
+  
+  def printer(self):
+    return f"This is {self._client} factura y debe pagar {self._total}"
+
+  
+google = Invoice("Google", "345")
+print(google.client)
