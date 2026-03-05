eed142956aea3c2730d9498678d60a8b01fb90e7
Sasha S.P. <sasa@bubulazy.com>
2026-03-05 18:22:14 +0100

    Flask API installation - Build sqlite schema and DB

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,13 +1,37 @@
-# Primera app - endpoint api en flask
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hey flask toma por culi lask"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Guide(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), unique=False)
+  content = db.Column(db.String(144), unique=False)
+
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
+
+class GuideSchema(ma.Schema):
+  class Meta:
+    field = ("title", "content")
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
 
+# ### ESO HAY QUE EJECUTAR EN TERMINAL PARA QUE SE CREA APP.SQLITE FILE Y SE CREA SQL SCHEMA
+# python3
+# from app import app, db
+# with app.app_context():
+#     db.create_all()
